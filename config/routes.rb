6e9a148736a8891b0ef866437a21dc1de9a3f04b
Simon Keryhuel <keryhuel.simon@gmail.com>
2019-03-19 11:50:09 +0100

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,28 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :costumess do
-  resources :bookings
+  devise_for :users
+  root to: 'pages#home'
+
+  # as locataire
+  resources :costumes, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
   end
 
+  resources :bookings, only: [:index]
 
+  # as both locataire and owner
+  resource :profile, only: [:show]
+
+  # as owner
+  namespace :owner do
+    resources :costumes, only: [:index, :new, :create]
+
+    resources :bookings, only: [:index] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
 end
-
-
