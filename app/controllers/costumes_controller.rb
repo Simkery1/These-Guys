4b93abd5352249fb6110e167b0cdb4fa56e39433
Jimmy Le Rouzic <jimmy.lerouzic@gmail.com>
2019-03-20 13:08:19 +0100

    feature index wip

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,5 +1,10 @@
 class CostumesController < ApplicationController
-  def index
-    @costumes = Costume.all
+  skip_before_action :authenticate_user!, only: :index
+
+   def index
+    # if params[:query].present?
+    #   @costumes = Costume.where("title ILIKE ?", "%#{params[:query]}%")
+    # else
+      @costumes = Costume.all
+    end
   end
-end
