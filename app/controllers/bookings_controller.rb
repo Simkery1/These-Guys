efe45337c82d042ae39bb7f8bcd95db04db7ef1d
Matthieu Quetin <matthieu.quetin@orange.fr>
2019-03-20 13:47:22 +0100

    rien fait

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,9 @@
 class BookingsController < ApplicationController
   before_action :set_booking
+  skip_before_action :authenticate_user!, only: :index
 
   def index
-    @booking = Booking.all
+    @bookings = Booking.all
   end
 
   def new
@@ -11,7 +12,7 @@ class BookingsController < ApplicationController
 
   private
 
-  def set_cocktail
+  def set_booking
     @booking = Booking.find(params[:id])
   end
 end
