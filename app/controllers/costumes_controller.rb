9e3017d16381853f2d88b63477b6ef1cf3c16f97
Matthieu Quetin <matthieu.quetin@orange.fr>
2019-03-20 12:05:57 +0100

    pardon jai push origin master

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,8 +1,7 @@
 class CostumesController < ApplicationController
-skip_before_action :authenticate_user!, only: :show
+  skip_before_action :authenticate_user!, only: :show
 
   def show
     @costume = Costume.find(params[:id])
   end
-
 end
